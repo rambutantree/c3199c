f4f1eb686dbd20bf367de182b077b3672c98e7a6
Tan Poh Keam <tan_poh_keam@rp.edu.sg>
2019-06-08 11:12:20 +0800

    Add files via upload

---
+++ lu14/js/ex14_2_solution.js
@@ -0,0 +1,17 @@
+// Use this file for the exercise
+
+
+//Insert the first block of codes here
+
+$('#p01').html('This paragraph cannot be  replace!');
+
+
+
+// inser the second block of code here
+
+$(document).ready(function(){
+$('#p01').html('This paragraph is replaced after the document is ready.');
+    });
+
+
+    
