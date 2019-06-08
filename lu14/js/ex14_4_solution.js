dbdef3ba8b329fb0e6233cc097928bb41e54fe29
Tan Poh Keam <tan_poh_keam@rp.edu.sg>
2019-06-08 12:09:32 +0800

    Add files via upload

---
+++ lu14/js/ex14_4_solution.js
@@ -0,0 +1,25 @@
+// Use this file for the exercise
+
+$(document).ready(function(){
+
+// sample event for a non-existing button
+$('#someBtn').click(function() {
+	console.log('You click on something');                                           
+	}); // close of button click
+    
+// Insert first button click event
+
+    
+    
+// Insert second button click event
+
+    
+// Insert third button click event
+
+    
+// Insert fourth button click event
+
+    
+    
+    }); // end of ready event
+// No codes beyond this point
